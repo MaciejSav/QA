297e8678e627ee940fba8f2866fadb4f95b33b60
MaciejSav <sawicki_maciej@wp.pl>
2019-11-19 12:18:37 +0100

    NewTest with Zadanie1, new ParameterizedTest

---
+++ QAJUnit/src/test/java/com/jsystems/qa/qajunit/NewTest.java
@@ -0,0 +1,29 @@
+package com.jsystems.qa.qajunit;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class NewTest {
+
+    @Test
+    public void zadanie1() {
+
+        String resultString = "Wordpress powers 34% of the internet";
+        String expectedString = "Wordpress powers [number]% of the internet";
+
+        assertTrue(resultString.startsWith("Wordpress powers"));
+        assertTrue(resultString.endsWith("% of the internet"));
+        assertThat(resultString).matches("(Wordpress powers )\\d+(% of the internet)");
+
+        String result = resultString.replace("Wordpress powers ", "").replace("% of the internet", "");
+        int resultNumber = Integer.parseInt(result);
+        assertTrue(resultNumber > 0 );
+    }
+}
