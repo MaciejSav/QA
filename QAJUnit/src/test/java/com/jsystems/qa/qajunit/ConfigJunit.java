ebfced624c5dc137bf1647fa5fb0e5b083ad22ac
MaciejSav <sawicki_maciej@wp.pl>
2019-11-19 12:24:39 +0100

    New Tag

--- QAJUnit/src/test/java/com/jsystems/qa/qajunit/ConfigJunit.java
+++ QAJUnit/src/test/java/com/jsystems/qa/qajunit/ConfigJunit.java
@@ -3,6 +3,7 @@ package com.jsystems.qa.qajunit;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 
+
 public class ConfigJunit {
     @BeforeAll
     public static void setupAll() {
--- QAJUnit/src/test/java/com/jsystems/qa/qajunit/NewTest.java
+++ QAJUnit/src/test/java/com/jsystems/qa/qajunit/NewTest.java
@@ -1,5 +1,6 @@
 package com.jsystems.qa.qajunit;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
@@ -10,6 +11,7 @@ import static com.google.common.truth.Truth.assertThat;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@Tag("Unit Test")
 public class NewTest {
 
     @Test
