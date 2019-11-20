b62f99d8b01a61a2dc85fe35ab789ec2c587be74
MaciejSav <sawicki_maciej@wp.pl>
2019-11-20 15:29:50 +0100

    notifi test

--- QAGui/src/test/java/com/jsystems/qa/qagui/FrontendTest.java
+++ QAGui/src/test/java/com/jsystems/qa/qagui/FrontendTest.java
@@ -16,12 +16,12 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 import static com.google.common.truth.Truth.assertThat;
-import static com.jsystems.qa.qagui.page.LoginPage.primaryButtonSelector;
-import static com.jsystems.qa.qagui.page.LoginPage.usernameOrEmailSelector;
+import static com.jsystems.qa.qagui.page.LoginPage.*;
 import static com.jsystems.qa.qagui.page.MainWordpressPage.loginIconSelector;
-import static com.jsystems.qa.qagui.page.UserPage.userAvatarSelector;
-import static com.jsystems.qa.qagui.page.UserPage.userDisplayNameSelector;
+import static com.jsystems.qa.qagui.page.UserPage.*;
+import static com.jsystems.qa.qagui.page.UserPage.primaryButtonSelector;
 import static java.lang.Thread.sleep;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
@@ -109,12 +109,59 @@ public class FrontendTest extends ConfigFrontend {
         assertTrue(userPage.saveUserDetailsButton.isDisplayed());
         assertTrue(!userPage.saveUserDetailsButton.isEnabled());
 
+
+
 //        najczestsze metody do sprawdzania na stronie
 
 //        assertTrue(saveUserDetailsButton.isDisplayed());
 //        assertTrue(saveUserDetailsButton.isEnabled());
 //        assertTrue(saveUserDetailsButton.isSelected())
 
+
+    }
+
+    @Test
+    public void notificationTest() {
+
+        driver.navigate().to(Configuration.BASE_URL);
+        MainWordpressPage mainWordpressPage = new MainWordpressPage(driver);
+
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(loginIconSelector)));
+
+        wait.until(ExpectedConditions.elementToBeClickable(mainWordpressPage.loginIcon));
+        mainWordpressPage.loginIcon.click();
+
+        LoginPage loginPage = new LoginPage(driver);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(usernameOrEmailSelector)));
+        loginPage.usernameInput.clear();
+        loginPage.usernameInput.sendKeys(Configuration.LOGIN);
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(primaryButtonSelector)));
+        loginPage.usernameButton.click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.id(passwordInputSelector)));
+        loginPage.inputPassword.clear();
+        loginPage.inputPassword.sendKeys(Configuration.PASSWORD);
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(primaryButtonSelector)));
+        loginPage.usernameButton.click();
+
+        UserPage userPage = new UserPage(driver);
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(userAvatarSelector)));
+        userPage.userAvatar.click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(userDisplayNameSelector)));
+
+        userPage.notificationSideLine.click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(commentSelector)));
+        userPage.commentTopLine.click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(firstCheckboxSelector)));
+        assertTrue(userPage.firstCheckbox.isSelected());
+
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(firstCheckboxSelector)));
+        userPage.firstCheckbox.click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(firstCheckboxSelector)));
+        assertFalse(userPage.firstCheckbox.isSelected());
+        userPage.firstCheckbox.click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(firstCheckboxSelector)));
+        assertTrue(userPage.firstCheckbox.isSelected());
 
     }
 
