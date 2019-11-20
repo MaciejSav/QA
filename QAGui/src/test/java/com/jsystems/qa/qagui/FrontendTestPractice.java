e608f864caee5cda5a359313eef3af9ff0cc5d89
MaciejSav <sawicki_maciej@wp.pl>
2019-11-20 16:00:05 +0100

    clean and practice frontend test

--- QAGui/src/test/java/com/jsystems/qa/qagui/FrontendTestPractice.java
+++ QAGui/src/test/java/com/jsystems/qa/qagui/FrontendTestPractice.java
@@ -26,17 +26,17 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 @Tag("FrontTest")
-public class FrontendTest extends ConfigFrontend {
-
-    // clean version
+public class FrontendTestPractice extends ConfigFrontend {
 
     @Test
     public void frontTest() {
         driver.navigate().to(Configuration.BASE_URL);
+//        WebElement textElement_1 = driver.findElement(By.cssSelector("h1.lpc-headline-title span:nth-child(1)"));
         MainWordpressPage mainWordpressPage = new MainWordpressPage(driver);
         String text1 = mainWordpressPage.textElement_1.getText();
         assertTrue(text1.equals("WordPress powers"));
 
+//        WebElement textElement_2 = driver.findElement(By.cssSelector("h1.lpc-headline-title span:nth-child(2)"));
         String text2 = mainWordpressPage.textElement_2.getText();
         assertTrue(text2.contains("% of the internet."));
         assertThat(text2).matches("\\d+(% of the internet.)");
@@ -49,52 +49,74 @@ public class FrontendTest extends ConfigFrontend {
         driver.navigate().to(Configuration.BASE_URL);
         MainWordpressPage mainWordpressPage = new MainWordpressPage(driver);
 
+//        String loginIconSelector = ".x-nav-item.x-nav-item--wide.x-nav-item--logged-in";
         WebDriverWait wait = new WebDriverWait(driver, 30);
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(loginIconSelector)));
 
+//        WebElement loginIcon = driver.findElement(By.cssSelector(loginIconSelector));
         wait.until(ExpectedConditions.elementToBeClickable(mainWordpressPage.loginIcon));
 
         mainWordpressPage.loginIcon.click();
 
         LoginPage loginPage = new LoginPage(driver);
 
+//        String usernameOrEmailSelector = "usernameOrEmail";
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(usernameOrEmailSelector)));
 
+//        WebElement usernameInput = driver.findElement(By.id(usernameOrEmailSelector));
 
         loginPage.usernameInput.clear();
         loginPage.usernameInput.sendKeys(Configuration.LOGIN);
 
+//        String primaryButtonSelector = ".button.form-button.is-primary";
         wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(primaryButtonSelector)));
 
+//        WebElement usernameButton = driver.findElement(By.cssSelector(primaryButtonSelector));
         loginPage.usernameButton.click();
 
-        wait.until(ExpectedConditions.elementToBeClickable(By.id("password")));;
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("password")));
+//        WebElement inputPassword = driver.findElement(By.id("password"));
 
         loginPage.inputPassword.clear();
         loginPage.inputPassword.sendKeys(Configuration.PASSWORD);
 
         wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(primaryButtonSelector)));
-
+//        WebElement buttonPassword = driver.findElement(By.cssSelector(primaryButtonSelector));
         loginPage.usernameButton.click();
 
         UserPage userPage = new UserPage(driver);
 
+//        String userAvatarSelector = ".masterbar__item.masterbar__item-me";
         wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(userAvatarSelector)));
 
+//        WebElement userAvatar = driver.findElement(By.cssSelector(userAvatarSelector));
         userPage.userAvatar.click();
+
+//        String userDisplayNameSelector = ".profile-gravatar__user-display-name";
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(userDisplayNameSelector)));
 
+//        WebElement userDisplayName = driver.findElement(By.cssSelector(userDisplayNameSelector));
         String userDisplayNameText = userPage.userDisplayName.getText();
 
         assertThat(userDisplayNameText).isEqualTo(Configuration.LOGIN);
 
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(primaryButtonSelector)));
+//        WebElement saveUserDetailsButton = driver.findElement(By.cssSelector(primaryButtonSelector));
 
         assertTrue(userPage.saveUserDetailsButton.isDisplayed());
         assertTrue(!userPage.saveUserDetailsButton.isEnabled());
+
+
+
+//        najczestsze metody do sprawdzania na stronie
+
+//        assertTrue(saveUserDetailsButton.isDisplayed());
+//        assertTrue(saveUserDetailsButton.isEnabled());
+//        assertTrue(saveUserDetailsButton.isSelected())
+
 
     }
 
