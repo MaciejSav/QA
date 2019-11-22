6350da993263be1ef5a9e809d08d3ae1ba2099b6
MaciejSav <sawicki_maciej@wp.pl>
2019-11-22 10:10:22 +0100

    full Api Tests update

--- QAapi/src/test/java/com/jsystems/qa/qaapi/ApiTest.java
+++ QAapi/src/test/java/com/jsystems/qa/qaapi/ApiTest.java
@@ -1,14 +1,22 @@
 package com.jsystems.qa.qaapi;
 
-import com.jsystems.qa.qaapi.model.User;
+import com.jsystems.qa.qaapi.database.UserDao;
+import com.jsystems.qa.qaapi.model.azure.AzureAuthor;
+import com.jsystems.qa.qaapi.model.device.User;
+import com.jsystems.qa.qaapi.model.error.ErrorResponse;
+import com.jsystems.qa.qaapi.model.user.MyUser;
+import com.jsystems.qa.qaapi.model.user.UserAzure;
+import com.jsystems.qa.qaapi.model.user.UserDb;
 import com.jsystems.qa.qaapi.service.UserService;
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static com.google.common.truth.Truth.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -31,8 +39,8 @@ public class ApiTest {
     }
 
     @Test
-    @DisplayName("Second api test")
-    public void shouldReturnListOfUsers() {
+    @DisplayName("Should returns correctly list of users")
+    public void shouldReturnsListOfUsers() {
         RestAssured
                 .given()
                 .get("http://www.mocky.io/v2/5a6a58222e0000d0377a7789")
@@ -41,24 +49,87 @@ public class ApiTest {
                 .statusCode(200)
                 .body("[0].imie", notNullValue())
                 .body("[0].imie", equalTo("Piotr"))
-                .body("[0].nazwisko", notNullValue())
-                .body("[0].nazwisko", equalTo("Kowalski"))
-//                .body("[0].device[0].device.model", equalTo("dell") - to nie przejdzie, bo . oznacza kolejne zagniezdzenie
+                .body("[0].nazwisko",notNullValue())
+                .body("[0].nazwisko",equalTo("Kowalski"))
+//                .body("[0].device[0].device.model.produce", equalTo("dell"))
         ;
-
     }
 
-
     @Test
-    @DisplayName("Should return correctly list of users using jsonPath mapping")
+    @DisplayName("Should returns correctly list of users using jsonPath mapping")
     public void jsonPathTest() {
         List<User> users = UserService.getUsers();
 
         assertTrue(users.get(0).imie.equals("Piotr"));
         assertTrue(users.get(0).nazwisko.equals("Kowalski"));
-        assertTrue(users.get(0).device.get(0).type.equals(("computer")));
+        assertTrue(users.get(0).device.get(0).type.equals("computer"));
         assertTrue(users.get(0).device.get(0).deviceModel.get(0).screenSize == 17);
         assertTrue(users.size() > 0);
-
     }
+
+    @Test
+    @DisplayName("Test with mapped of MyUser")
+    public void myUserJsonPath() {
+        MyUser myUserWithQuery = UserService.getUserWithQueryParam("Piotr", "Kowalski");
+        MyUser myUserWitrhPathVariable = UserService.getUserWithPathVariable(2L,  5);
+        MyUser myUser = UserService.getUser();
+
+        assertThat(myUser.name).isEqualTo("Piotr");
+        assertThat(myUser.surname).isEqualTo("Kowalski");
+
+        assertThat(myUserWithQuery.name).isEqualTo("Piotr");
+    }
+
+    @Test
+    @DisplayName("Post test")
+    public void postTest() {
+        String[] strings = UserService.postMyUser(new MyUser("Piotr", "Kowalski"));
+        assertThat(strings).isEmpty();
+    }
+
+    @Test
+    @DisplayName("Error response")
+    public void errorResponse() {
+        ErrorResponse errorResponse = UserService.getErrorResponse();
+
+        assertThat(errorResponse.error.errorCode).isEqualTo(400);
+        assertThat(errorResponse.error.validationError).isEqualTo("invalid_email");
+        assertThat(errorResponse.error.message).isEqualTo("your email is invalid");
+    }
+
+    @Test
+    public void azureUser(){
+
+        UserAzure userAzure = UserService.getUserAzureById(1);
+
+        assertThat(userAzure.id).isEqualTo(1);
+        assertThat(userAzure.userName).isEqualTo("User 1");
+        assertThat(userAzure.password).isEqualTo("Password1");
+    }
+
+    @Test
+    @Disabled
+    public void dbTest() {
+        UserDb userDb = UserDao.getOneById(1L);
+        assertThat(userDb.getName()).isEqualTo("Piotr");
+    }
+
+    @Test
+    @DisplayName("Get azure authors")
+    public void shouldReturnsAllAzureAuthorsList() {
+        List<AzureAuthor> azureAuthors = UserService.getAzureAuthors();
+
+        assertThat(azureAuthors.size()).isGreaterThan(0);
+
+        for (AzureAuthor azureAuthor : azureAuthors) {
+            int firstNameId = Integer.parseInt(azureAuthor.firstName.replace("First Name", ""));
+            assertThat(azureAuthor.firstName).contains("FirstName ");
+            assertThat(azureAuthor.firstName).matches("FirstName \\d*");
+            assertTrue(azureAuthor.id == firstNameId);
+        }
+    }
+
+
+
+
 }
